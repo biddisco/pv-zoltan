ba8ea6a57f3e3da9d1d20b2821e9f7d736616583
John Biddiscombe <biddisco@cscs.ch>
2016-03-02 22:56:50 +0100

    Improvements to UnstructuredGrid wavelet test

--- scripts/benchmark-wavelet-UG.py
+++ scripts/benchmark-wavelet-UG.py
@@ -31,7 +31,6 @@ for o, a in opts:
     else:
         assert False, "unhandled option" + str(o) + " " + str(a)
 
-# help(servermanager.vtkProcessModule.GetProcessModule())
 nranks = servermanager.vtkProcessModule.GetProcessModule().GetNumberOfLocalPartitions()
 rank   = servermanager.vtkProcessModule.GetProcessModule().GetPartitionId()
 print ('Number of processes ' + str(nranks) + ' this is rank ' + str(rank))
@@ -43,7 +42,7 @@ paraview.simple._DisableFirstRenderCameraReset()
 # Pipeline
 ##############
 
-numcells = 150*150*150*nranks
+numcells = 100*100*100*nranks
 sidelength = 1 + int(math.pow(numcells, 1.0/3.0)/2)
 print("Wavelet size {-" + str(sidelength) + "," + str(sidelength) + "}") 
 print("Wavelet numcells " + str(8*sidelength*sidelength*sidelength)) 
@@ -57,23 +56,28 @@ wavelet1.WholeExtent = [-waveletside, waveletside, -waveletside, waveletside, -w
 # create a new 'Tetrahedralize'
 tetrahedralize1 = Tetrahedralize(Input=wavelet1)
 
+filter2 = tetrahedralize1
+
 # create a new 'Particle Partition Filter'
 if (filter==0):
-  partitionFilter1 = ParticlePartitionFilter(Input=tetrahedralize1)
-  partitionFilter1.WeightsScalarArray = ''
+  partitionFilter1 = MeshPartitionFilter(Input=filter2)
+  partitionFilter1.PointWeightsArrayName
   partitionFilter1.KeepInversePointLists = 0
   partitionFilter1.MaxAspectRatio = 5
   partitionFilter1.GhostHaloSize = ghostoverlap
 else:
-  partitionFilter1 = D3(Input=tetrahedralize1)
+  partitionFilter1 = D3(Input=filter2)
   partitionFilter1.MinimalMemory = 1
   partitionFilter1.BoundaryMode = 'Assign cells uniquely'
+  
+filter3 = partitionFilter1
 
 # create a new 'Shrink'
-shrink1 = Shrink(Input=partitionFilter1)
+shrink1 = Shrink(Input=filter3)
 # Properties modified on shrink1
 shrink1.ShrinkFactor = 0.9
 
+filter4 = shrink1
 
 ##################
 # colour table
@@ -100,10 +104,10 @@ rTDataLUT.EnableOpacityMapping = 1
 
 renderView1 = GetActiveViewOrCreate('RenderView')
 # uncomment following to set a specific view size
-renderView1.ViewSize = [640, 480]
+renderView1.ViewSize = [1024, 1024]
 
 # show data in view
-shrink1Display = Show(shrink1, renderView1)
+shrink1Display = Show(filter4, renderView1)
 # trace defaults for the display properties.
 shrink1Display.ColorArrayName = ['POINTS', 'RTData']
 shrink1Display.LookupTable = rTDataLUT
@@ -112,18 +116,19 @@ shrink1Display.LookupTable = rTDataLUT
 #renderView1.ResetCamera()
 
 # show color bar/color legend
-shrink1Display.SetScalarBarVisibility(renderView1, True)
+#shrink1Display.SetScalarBarVisibility(renderView1, True)
 
 #### saving camera placements for all active views
 
 # current camera placement for renderView1
-renderView1.CameraPosition = [-129.07375961872597, 417.62235377155565, 143.9824442336569]
+distance = sidelength*math.sqrt(math.sqrt(2.0)*10.0)
+renderView1.CameraPosition = [distance, distance, distance]
 renderView1.CameraViewUp = [0.7306437409080044, 0.4132080413165191, -0.5435244598574407]
 renderView1.CameraParallelScale = 121.11167448863597
 
 #### uncomment the following to render all views
 #RenderAllViews()
 #alternatively, if you want to write images, you can use 
-SaveScreenshot("wavelet-1.png")
+SaveScreenshot("wavelet-filter-"+str(filter)+".png")
 #
 stats.dump_stats()
